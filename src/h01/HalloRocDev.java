997b4bdec91f65b21b9029874aa829128ec8448a
dedaveking2 <dave.witte@live.nl>
2015-08-21 12:02:16 +0200

    Update HalloRocDev.java

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -5,7 +5,7 @@ public class HalloRocDev {
 	public static void main(String[] args) {
 		System.out.println("Hallo roc-dev!");
 		
-		sytem.out.println("Hello it is me Dave!!!");
+		System.out.println("Hello it is me Dave!!!");
 		
 	}
 
