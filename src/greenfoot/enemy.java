e3b65d2a976a5334dcae196c21ccc9eb58c15632
dave <dave@10.68.205.70>
2015-11-20 09:09:59 +0100

    uuuhhhgggggg

--- src/greenfoot/enemy.java
+++ src/greenfoot/enemy.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Gnomus extends Actor
+public class enemy extends Actor
 {
     /**
      * Act - do whatever the Gnomus wants to do. This method is called whenever
--- src/greenfoot/world.java
+++ src/greenfoot/world.java
@@ -18,8 +18,24 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 500, 1); 
         
-       
         addObject(new link(), 175, 250);
-        addObject(new Gnomus(), 475, 250);
+        addObject(new enemy(), Greenfoot.getRandomNumber(600), Greenfoot.getRandomNumber(500));
+        
+        for(int teller1 = 0; teller1 <= 5; teller1++)
+        {
+            addObject(new bom(), Greenfoot.getRandomNumber(600), Greenfoot.getRandomNumber(500));
+        }
+        
+        for(int teller2 = 0; teller2 <= 6; teller2++)
+        {
+            addObject(new rock(), Greenfoot.getRandomNumber(600), Greenfoot.getRandomNumber(500));
+        }
+        
+        for(int teller3 = 0; teller3 <= 10; teller3++)
+        {
+            addObject(new rupee(), Greenfoot.getRandomNumber(600), Greenfoot.getRandomNumber(500));
+        }
+        
+        setPaintOrder(link.class, enemy.class, rupee.class, bom.class, rock.class);
     }
 }
